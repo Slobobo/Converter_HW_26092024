d2dd2599b4356ac587c6fa25d6fdc1366c88c23a
Dmytro Slobodian <Slobodiyan@gmail.com>
2024-09-29 15:40:09 +0300

    Add farengate conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,11 +6,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter App.");
         double farengate = 94;
-        double celsiu = convFarengateToCelsiu(farengate);
-        System.out.println("Result is " + celsiu + " celsiu.");
+        double celsiu = 18;
+
+        double celsiuConversion = convFarengateToCelsiu(farengate);
+        double farengateConversion = convCelsiuToFarengate(celsiu);
+
+        System.out.println(farengate + " farengate equilse " + celsiuConversion + " celsium.");
+        System.out.println(celsiu + " celsiu equilse " + farengateConversion + " farengate.");
     }
 
     private static double convFarengateToCelsiu(double farengate) {
         return (farengate - 32) * 5 / 9;
+    }
+    private static double convCelsiuToFarengate(double celsiu) {
+        return (celsiu * 9 / 5) + 32;
     }
 }
